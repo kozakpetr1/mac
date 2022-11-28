cfba54c1fcc3f62b8731b4723cb7f250de445f51
kozakpetr1 <kozak.petr@gmail.com>
2022-11-28 13:57:42 +0100

    instalace SW na Mac

---
+++ mariadb.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/skolavdf/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/skolavdf/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+/opt/homebrew/bin/brew install mariadb
+# cd /opt/homebrew/Cellar/mariadb/10.10.2/bin
+/opt/homebrew/bin/brew services restart mariadb
+/opt/homebrew/bin/brew install php
+/opt/homebrew/bin/brew install node
+
+
